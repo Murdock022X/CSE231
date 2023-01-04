d113e7292e38decefcb0c547c032c5d646e43dcf
Henry Gerald Murdock <107006226+Murdock022X@users.noreply.github.com>
2023-01-03 23:52:21 -0500

    Add files via upload

---
+++ Project4/proj04.py
@@ -0,0 +1,30 @@
+
+DELTA = 10**-7  #used for zeta f(x)
+
+PROMPT = "Enter Z for Zeta, C for Conway, Q to quit: "
+
+def  int_to_base13(n):
+    '''Add DocString Here'''
+    pass
+
+def tridecimal_expansion(n_str):
+    '''Add DocString Here'''
+    pass
+
+def tridecimal_to_conway(n_str):
+    '''Add DocString Here'''
+    pass
+
+def zeta(n):
+    '''Add DocString Here'''
+    pass
+
+def main():
+    #by convention main doesn't need a docstring
+    
+    print('Functions')
+    #code goes here
+    print('\nThank you for playing')
+    
+if __name__ == '__main__':
+    main()
